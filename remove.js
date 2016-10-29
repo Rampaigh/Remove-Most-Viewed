31f140083bdf978b3ae1104f7ca9c2ac81a5d967
Rampaigh <rampaigh@gmail.com>
2016-10-29 12:32:00 +0200

    v1.1 - Removed dead space below search bar

--- remove.js
+++ remove.js
@@ -1,4 +1,6 @@
 (function() {
   var element = document.getElementById('most-visited');
   element.parentNode.removeChild(element);
+  document.body.style.minHeight = '0';
+  document.body.style.height = 'auto';
 })();
